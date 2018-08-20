b30e75917f9fc678f7b1df828eac1e0cd2e3e914
Viam Mercer <viam.mercer@hotmail.com>
2018-08-20 14:28:46 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,24 +1,24 @@
-var chocolateBars = ["snickers","hundred grand","kitkat","skittles"]
+var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
 
 
 function addElementToBeginningOfArray(array, element) {
-  array = [element, ...array]
-  return array
+  array = [element, ...array];
+  return array;
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift(element)
-  return array
+  array.unshift(element);
+  return array;
 }
 
 function addElementToEndOfArray(array, element) {
-  array = [...array, element]
-  return array
+  array = [...array, element];
+  return array;
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-  array.push(element)
-  return array 
+  array.push(element);
+  return array;
 }
 
 function accessElementInArray(array, index) {
@@ -44,3 +44,4 @@ function removeElementFromEndOfArray(array) {
   array = array.slice(0, array.length - 1);
   return array;
 }
+
